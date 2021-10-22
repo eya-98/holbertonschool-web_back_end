354e15e1c015d09230fb3261ebc2b6ef2d5198e6
eya-98 <eya.nani@sesame.com.tn>
2021-10-22 14:53:23 +0100

    sum

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function sum_list"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    sum: float = 0
+    for i in input_list:
+        sum += i
+    return sum
