eaf195486faeee09d489ef071c82fbbfa8c9d081
eya-98 <eya.nani@sesame.com.tn>
2021-10-22 14:06:19 +0100

    floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """type-annotated function floor"""
-import math
 
 
 def floor(n: float) -> float:
     """returns the floor of the float."""
 
-    return math.floor(n)
+    return int(n)
