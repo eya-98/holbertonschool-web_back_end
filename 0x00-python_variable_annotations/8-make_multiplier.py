dee032940f2b01f02429bedcfe2b83137cf63548
eya-98 <eya.nani@sesame.com.tn>
2021-10-27 15:17:26 +0100

    make multiplier

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a type-annotated function make_multiplier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[int, float], float]:
+    def multiply(x):
+        return x * multiplier
+    return multiply
