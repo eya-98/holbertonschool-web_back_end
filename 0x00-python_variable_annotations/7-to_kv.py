aabb1fd17d5eb6c81e2bd3fc3bddca0b413d6873
eya-98 <eya.nani@sesame.com.tn>
2021-10-27 11:09:37 +0100

    import Tuple and Union

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,4 +4,5 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """returns a tuple of an str and sqr of a float or int"""
     return (k, v ** 2)
