e048c8dc86ee908653d3b5a8bf8107e3826410e7
eya-98 <eya.nani@sesame.com.tn>
2021-10-30 14:03:37 +0100

    Sequence and Iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -4,4 +4,5 @@ from typing import List, Iterable, Sequence, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """add the appropriate types"""
     return [(i, len(i)) for i in lst]
