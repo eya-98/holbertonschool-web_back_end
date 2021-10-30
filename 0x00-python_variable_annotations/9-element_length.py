96ab0d0d1a3f742ed076449b66e710d0f95f2911
eya-98 <eya.nani@sesame.com.tn>
2021-10-30 14:01:55 +0100

    Sequence and Iterable

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Annotate the below function’s parameters"""
+from typing import List, Iterable, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    return [(i, len(i)) for i in lst]
