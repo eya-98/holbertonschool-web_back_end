be8bd65a6ee84d6ed6d4b500c18dd28763c7758e
eya-98 <eya.nani@sesame.com.tn>
2021-11-08 16:40:31 +0100

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,7 +5,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
-    lists = []
-    async for i in async_generator():
-        lists.append(i)
-    return lists
+    return [i async for i in async_generator()]
