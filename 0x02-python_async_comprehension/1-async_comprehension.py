cb5baeb9c34e8c91ab8be56822d3a9bd9fea0056
eya-98 <eya.nani@sesame.com.tn>
2021-11-08 16:42:28 +0100

    async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """a coroutine called async_comprehension"""
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
+    """async comprehension"""
     return [i async for i in async_generator()]
