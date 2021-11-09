66ad7c64a0127d5e38f1d157014624d62d8d457b
eya-98 <eya.nani@sesame.com.tn>
2021-11-09 16:22:44 +0100

    fifo cach

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""FIFOCache that inherits from BaseCaching and is a caching system:"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """a class FIFOCache"""
+    def __init__(self):
+        """init function"""
+        super().__init__()
+
+    def put(self, key, item):
+        """add item or update it"""
+        if key is None or item is None:
+            pass
+        elif len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS \
+                and key not in self.cache_data.keys():
+            print('DISCARD:', end='')
+            print(list(self.cache_data)[0])
+            self.cache_data.pop(list(self.cache_data)[0])
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """"get item from the cache dict"""
+        if key is None:
+            pass
+        elif key not in self.cache_data.keys():
+            pass
+        elif key in self.cache_data:
+            return self.cache_data[key]
