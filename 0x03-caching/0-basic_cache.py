e9bbb5f840ecf9afa98337e9dd68c59b5bcca42d
eya-98 <eya.nani@sesame.com.tn>
2021-11-09 15:43:46 +0100

    basic cache

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -5,13 +5,16 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """that inherits from BaseCaching and is a caching system"""
+    
     def put(self, key, item):
+        """add item to the cache dict"""
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """"get item from the cache dict"""
         if key is None:
             pass
         elif key not in self.cache_data.keys():
