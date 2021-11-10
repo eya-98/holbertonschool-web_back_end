36819008493a9dc2c987bde6ec1cbc3c82e88eb5
eya-98 <eya.nani@sesame.com.tn>
2021-11-10 22:24:55 +0100

    lifo

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -13,14 +13,14 @@ class LIFOCache(BaseCaching):
         """put method"""
         if key is not None and item is not None:
             length = len(self.cache_data.keys())
-            if length + 1 > BaseCaching.MAX_ITEMS /
-            and key not in self.cache_data.keys():
+            if length + 1 > BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
                 print('DISCARD:', end='')
                 print(list(self.cache_data)[length - 1])
                 self.cache_data.pop(list(self.cache_data)[length - 1])
             if key in self.cache_data.keys():
-                self.cache_data.pop(list /
-                                    (self.cache_data) /
+                self.cache_data.pop(list
+                                    (self.cache_data)
                                     [list(self.cache_data).index(
                                         '{}'.format(key))])
             self.cache_data[key] = item
