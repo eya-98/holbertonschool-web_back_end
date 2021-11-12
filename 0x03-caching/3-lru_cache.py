8af163c8d348f4c8d4861ef257468c139d669ca2
eya-98 <eya.nani@sesame.com.tn>
2021-11-12 14:18:01 +0100

    lru

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -1,41 +1,32 @@
 #!/usr/bin/env python3
 """Create a class LRUCache that inherits from BaseCaching"""
+from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
     """LRU class"""
-    keysList = []
 
     def __init__(self):
         """init function"""
         super().__init__()
-
-    def put(self, key, item):
-        """put method"""
-        if key is not None and item is not None:
-            if key in self.cache_data.keys():
-                self.keysList.pop(self.keysList.index(key))
-            if length + 1 > BaseCaching.MAX_ITEMS \
-                    and key not in self.cache_data.keys():
-                print('DISCARD:', end='')
-                print(self.keysList[0])
-                self.cache_data.pop(
-                    list(
-                        self.cache_data)[
-                        list(
-                            self.cache_data).index(
-                            '{}'.format(
-                                self.keysList[0]))])
-                self.keysList.pop(0)
-            self.cache_data[key] = item
-            self.keysList.append(key)
+        self.cache_data = OrderedDict()
 
     def get(self, key):
         """get method"""
         if key is None or key not in self.cache_data.keys():
             return None
         else:
-            self.keysList.pop(self.keysList.index(key))
-            self.keysList.append(key)
+            self.cache_data.move_to_end(key)
             return self.cache_data[key]
+
+    def put(self, key, item):
+        """put method"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                print('DISCARD:', end='')
+                print(list(self.cache_data.keys())[0])
+                self.cache_data.popitem(last=False)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key)
