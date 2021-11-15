1bb892e04ad18e08446cdd089f53ab364bd63da6
eya-98 <eya.nani@sesame.com.tn>
2021-11-15 21:38:00 +0100

    simple helper

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Write a function named index_range"""
+
+def index_range(page: int, page_size: int):
+    a = (page - 1) * page_size
+    b = page * page_size
+    return (a, b)
