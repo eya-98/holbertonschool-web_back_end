6ed12647f1ed423bcf90945790dd138b76101273
eya-98 <eya.nani@sesame.com.tn>
2021-11-16 13:50:52 +0100

    simple pagination

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""Write a function named index_range"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int):
+    """index range"""
+    a = (page - 1) * page_size
+    b = page * page_size
+    return (a, b)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) is int and type(page_size) is int
+        if page * page_size > len(self.dataset()) or page * page_size < 0:
+            return []
+        else:
+            index_start = index_range(page, page_size)[0]
+            index_end = index_range(page, page_size)[1]
+            return (self.__dataset[index_start:index_end])
