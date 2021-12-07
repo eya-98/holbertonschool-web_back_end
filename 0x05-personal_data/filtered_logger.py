14470dac1299cf0b62823d7736d0e1e7a9ca51f0
eya-98 <eya.nani@sesame.com.tn>
2021-12-07 15:17:08 +0100

    filtered logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,10 +4,18 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List, radaction: str, message: str, separator: str) -> str:
+def filter_datum(
+        fields: List,
+        radaction: str,
+        message: str,
+        separator: str) -> str:
     """returns the log message obfuscated"""
     for i in range(len(fields)):
-        pattern = re.search('{}=[^{}]*'.format(fields[i], separator), message).group()
-        replace = re.sub('=[^{}]*'.format(separator), '={}'.format(radaction), pattern)
+        pattern = re.search(
+            '{}=[^{}]*'.format(fields[i], separator), message).group()
+        replace = re.sub(
+            '=[^{}]*'.format(separator),
+            '={}'.format(radaction),
+            pattern)
         message = re.sub(pattern, replace, message)
     return (message)
