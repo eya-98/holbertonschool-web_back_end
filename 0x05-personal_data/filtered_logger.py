c2d05610503bf1d1ed3fd87c38bc9392c0a02fbe
eya-98 <eya.nani@sesame.com.tn>
2021-12-07 15:09:51 +0100

    filtered logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -4,20 +4,13 @@ import re
 
 
 def filter_datum(fields, radaction, message, separator):
-    dic = {}
-    field = [re.search('.*?=', message).group()]
-    field += re.findall('{}.*?='.format(separator), message)
-    value = re.findall('=.*?{}'.format(separator), message)
-    for i in range(len(field)):
-        field[i] = field[i][:-1]
-        if i != 0:
-            field[i] = field[i][1:]
-        value[i] = value[i][1:]
-        value[i] = value[i][:-1]
-        dic[field[i]] = value[i]
-    for i in fields:
-        dic[i] = radaction
-    new_string = ''
-    for key in dic.keys():
-        new_string += '{}'.format(key) + '=' + '{}'.format(dic[key]) + ';'
-    return new_string
+    """returns the log message obfuscated"""
+    for i in range(len(fields)):
+        pattern = re.search(
+            '{}=[^{}]*'.format(fields[i], separator), message).group()
+        replace = re.sub(
+            '=[^{}]*'.format(separator),
+            '={}'.format(radaction),
+            pattern)
+        message = re.sub(pattern, replace, message)
+    return (message)
