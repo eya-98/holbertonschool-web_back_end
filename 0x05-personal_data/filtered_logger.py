6bae0fe2382aeda82429c94f7fc369a703fb02a3
eya-98 <eya.nani@sesame.com.tn>
2021-12-21 13:47:14 +0100

    filter logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -13,17 +13,15 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
+        """init function"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """format function"""
         message = logging.Formatter(self.FORMAT).format(record)
-        return filter_datum(
-            self.fields,
-            self.REDACTION,
-            message,
-            self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
 
 
 def filter_datum(
