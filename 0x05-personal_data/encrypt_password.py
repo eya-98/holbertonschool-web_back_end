caa400f9c34c60fbfd80a6ffa6fd44facdb13f69
eya-98 <eya.nani@sesame.com.tn>
2021-12-21 16:26:14 +0100

    encrypt password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     password = password.encode()
     encryptedPassword = bcrypt.hashpw(password, bcrypt.gensalt())
     return (encryptedPassword)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """returns valid or not valid password"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
