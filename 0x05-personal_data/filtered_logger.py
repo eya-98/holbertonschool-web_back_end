425209c19c8760904eccab8a5645f495d0accca2
eya-98 <eya.nani@sesame.com.tn>
2021-12-21 13:30:32 +0100

    filtered logger

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 """a function called filter_datum"""
+from _typeshed import Self
 import re
 from typing import List
+import logging
 
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, message, self.SEPARATOR)
 
 def filter_datum(
         fields: List[str],
