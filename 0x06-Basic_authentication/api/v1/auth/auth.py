5eb74a5351cd1ccf70bd537b430638dd8b467d25
eya-98 <eya.nani@sesame.com.tn>
2022-01-11 17:24:57 +0100

    request validation

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """public method that returns None"""
-        return None
+        if request is None or request.headers.get('Authorization') is None:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """public method that returns None"""
