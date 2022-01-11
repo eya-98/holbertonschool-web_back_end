cece8ec8ee8207c247b7c624a32e104316f963cc
eya-98 <eya.nani@sesame.com.tn>
2022-01-11 14:47:14 +0100

    unauthorized

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -21,7 +21,7 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def request_unauthorized():
+def unauthorized(error) -> str:
     """request unauthorized error"""
     return jsonify({"error": "Unauthorized"}), 401
 
--- 0x06-Basic_authentication/api/v1/views/index.py
+++ 0x06-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'],  strict_slashes=False)
+def unauthorized() -> str:
+    """
+    calling abort(401)
+    """
+    abort(401)
+    return jsonify({})
