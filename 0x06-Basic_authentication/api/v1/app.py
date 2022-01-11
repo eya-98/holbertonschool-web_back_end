fd7b4a3c9d19f221c71c15dac7cddd4ddcf7e5f1
eya-98 <eya.nani@sesame.com.tn>
2022-01-11 13:19:31 +0100

    edit

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,6 +20,10 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(401)
+def request_unauthorized():
+    """request unauthorized error"""
+    return jsonify({"error": "Unauthorized"}), 401
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
