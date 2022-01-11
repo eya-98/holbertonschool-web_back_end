4a564216a189e8d75fc9699501031804e2eb7289
eya-98 <eya.nani@sesame.com.tn>
2022-01-11 16:40:55 +0100

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,4 +1,4 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 from flask import request
 from typing import List, TypeVar
 """Manage the API authentication."""
@@ -10,11 +10,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """public method that returns False"""
         return False
-        
+
     def authorization_header(self, request=None) -> str:
         """public method that returns None"""
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """public method that returns None"""
         return None
