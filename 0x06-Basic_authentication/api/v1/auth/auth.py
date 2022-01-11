d98c6ce73aff03f0222a08f875d2cd22cc3e299f
eya-98 <eya.nani@sesame.com.tn>
2022-01-11 15:48:48 +0100

    define Auth class

---
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+from flask import request
+from typing import List, TypeVar
+"""Manage the API authentication."""
+
+
+class Auth:
+    """a class to manage the API authentication."""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """public method that returns False"""
+        return False
+        
+    def authorization_header(self, request=None) -> str:
+        """public method that returns None"""
+        return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """public method that returns None"""
+        return None
