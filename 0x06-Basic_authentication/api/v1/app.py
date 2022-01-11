09737b25b49e9c79e58251794f63148c5190d446
eya-98 <eya.nani@sesame.com.tn>
2022-01-11 17:27:47 +0100

    request validation

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -19,6 +19,22 @@ if getenv('AUTH_TYPE') == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+@app.before_request
+def before_request():
+    """
+    filter each request
+    """
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if auth is None or not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -37,24 +53,6 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """forbidden error handler"""
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request():
-    """
-    filter each request
-    """
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-    if auth is None or auth.require_auth(request.path, excluded_paths) is None:
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 if __name__ == "__main__":
