9089e068ef7c3ee52b627adb79f2bcb49e5601d3
eya-98 <eya.nani@sesame.com.tn>
2022-01-13 14:55:24 +0100

    basic auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from flask import request
 from typing import List, TypeVar
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -28,3 +29,17 @@ class BasicAuth(Auth):
                 string += authorization_header[i]
                 i += 1
         return string
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ returns the decoded value of a Base64 string"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            b64 = base64_authorization_header.encode('utf-8')
+            str_bytes = base64.b64decode(b64)
+            return str_bytes.decode('utf-8')
+        except Exception:
+            return None
