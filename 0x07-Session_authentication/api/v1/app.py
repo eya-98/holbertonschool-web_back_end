df279304c9e9d899961c852055baf6e6b5b87276
eya-98 <eya.nani@sesame.com.tn>
2022-01-15 22:21:31 +0100

    session auth

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -43,11 +43,11 @@ def before_request():
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
     if auth.authorization_header(
             request) is None and auth.session_cookie(request) is None:
         abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
     request.current_user = auth.current_user(request)
 
 
