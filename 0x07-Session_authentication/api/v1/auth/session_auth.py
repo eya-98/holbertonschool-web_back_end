2c19ea0a62f7892a5c47514b909384806f38f656
eya-98 <eya.nani@sesame.com.tn>
2022-01-15 21:36:11 +0100

    session auth

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
-"""class BasicAuth that inherits"""
+"""class SessionAuth that inherits"""
 from flask import request
 from typing import List, TypeVar, Tuple
 from api.v1.auth.auth import Auth
 from models.user import User
+import uuid
 import base64
 
 
 class SessionAuth(Auth):
     """Authentication mechanism"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session = str(uuid.uuid4())
+        self.user_id_by_session_id[session] = user_id
+        return session
