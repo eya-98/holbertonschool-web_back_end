318a3a31aa6b05408be33222247f4e7e1c160fe2
eya-98 <eya.nani@sesame.com.tn>
2022-01-16 12:49:46 +0100

    session auth

--- 0x07-Session_authentication/api/v1/views/session_auth.py
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -12,9 +12,9 @@ from os import getenv
 def session_auth() -> str:
     """create a session"""
     email = request.form.get("email", None)
-    password = request.form.get("password", None)
     if email is None or email == "":
         return jsonify({"error": "email missing"}), 400
+    password = request.form.get("password", None)
     if password is None or password == "":
         return jsonify({"error": "password missing"}), 400
     user = User.search({"email": email})
@@ -22,10 +22,9 @@ def session_auth() -> str:
         return jsonify({"error": "no user found for this email"}), 404
     if user[0].is_valid_password(password) is None:
         return jsonify({"error": "wrong password"}), 401
-    else:
-        from api.v1.app import auth
-        sessionId = auth.create_session(user[0].id)
-        cookie_name = getenv('SESSION_NAME')
-        session = jsonify(user[0].to_json())
-        session.set_cookie(cookie_name, sessionId)
-        return session
+    from api.v1.app import auth
+    sessionId = auth.create_session(user[0].id)
+    cookie_name = getenv('SESSION_NAME')
+    session = jsonify(user[0].to_json())
+    session.set_cookie(cookie_name, sessionId)
+    return session
