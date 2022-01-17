0b7ff415f0aeadcf7a77ed5833e2dc21ccecfc35
eya-98 <eya.nani@sesame.com.tn>
2022-01-17 23:17:20 +0100

    session exp

--- 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -12,29 +12,31 @@ from os import getenv
 
 class SessionExpAuth(SessionAuth):
     """ an expiration date to a Session ID"""
+
     def __init__(self):
         """init method"""
         self.session_duration = getenv("SESSION_DURATION")
         if self.session_duration is None:
             self.session_duration = 0
-        if self.session_duration.isdigit():
-            self.session_duration = int(self.session_duration)
+            self.session_duration = int(self.session_duration, 0)
         else:
             self.session_duration = 0
-        
+
     def create_session(self, user_id=None):
         """Create a Session ID """
         id = super().create_session(user_id)
         if id is None:
             return None
-        self.user_id_by_session_id[id] = {"user_id": user_id, "created_at": datetime.now()}
+        self.user_id_by_session_id[id] = {
+            "user_id": user_id, "created_at": datetime.now()}
         return id
+
     def user_id_for_session_id(self, session_id=None):
         """user_id_for_session_id"""
         if session_id is None:
             return None
         if session_id not in self.user_id_by_session_id.keys():
-            return None        
+            return None
         if self.session_duration <= 0:
             return self.user_id_by_session_id[session_id].get('user_id')
         if "created_at" not in self.user_id_by_session_id[session_id].keys():
