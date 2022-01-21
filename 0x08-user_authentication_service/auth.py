e67fa403cf6d9316cc4222671a16922d94383fb5
eya-98 <eya.nani@sesame.com.tn>
2022-01-21 22:59:22 +0100

    auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -26,12 +26,13 @@ class Auth:
     """Auth class"""
 
     def __init__(self):
-        """Init method"""
+        """Init METHOD"""
         self._db = DB()
 
     def register_user(
-            self, email: str, password: str) -> User:
-        """Register user in database"""
+            self,
+            email: str, password: str) -> User:
+        """Registers a user"""
         try:
             self._db.find_user_by(email=email)
             raise ValueError(
@@ -41,40 +42,47 @@ class Auth:
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates login"""
-        user = self._db.find_user_by(email=email)
-        if user:
-            if bcrypt.checkpw(
-                    password.encode('utf-8'),
-                    user.hashed_password):
-                return True
-        return False
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                if bcrypt.checkpw(
+                        password.encode('utf-8'),
+                        user.hashed_password):
+                    return True
+                return False
+        except Exception:
+            return False
 
     def create_session(self, email: str) -> str:
-        """Create a session
-        """
-        user = self._db.find_user_by(email=email)
-        if user:
-            session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
-            return session_id
-        return None
+        """Creates a session"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                session_id = _generate_uuid()
+                self._db.update_user(user.id, session_id=session_id)
+                return session_id
+        except Exception:
+            return None
 
     def get_user_from_session_id(self, session_id: str) -> User:
         """Get user from session id"""
-        user = self._db.find_user_by(session_id=session_id)
-        if user:
-            return user
-        return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            if user:
+                return user
+            return None
+        except Exception:
+            return None
 
     def destroy_session(self, user_id: int):
-        """Destroys user session"""
+        """Destroys session"""
         try:
             return self._db.update_user(user_id, session_id=None)
         except Exception:
             return None
 
     def get_reset_password_token(self, email: str) -> str:
-        """Generates token"""
+        """get reset password token"""
         try:
             user = self._db.find_user_by(email=email)
             reset_token = _generate_uuid()
@@ -84,8 +92,7 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """Updates password
-        """
+        """Updates password"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             self._db.update_user(user.id,
