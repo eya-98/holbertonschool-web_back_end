31eab4568b08a6438ad74917f3400dfaf60fe287
eya-98 <eya.nani@sesame.com.tn>
2022-01-21 22:10:03 +0100

    add user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,35 +4,35 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-
-from user import Base, User
+from user import User, Base
 
 
 class DB:
-    """DB class
-    """
+    """ DB class"""
 
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self) -> Session:
+    def _session(self):
         """Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
-        """add user"""
-        user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
-        return user   
+        """
+        add user
+        """
+        DBSession = self._session
+        new_user = User(email=email, hashed_password=hashed_password)
+        DBSession.add(new_user)
+        DBSession.commit()
+        return new_user
