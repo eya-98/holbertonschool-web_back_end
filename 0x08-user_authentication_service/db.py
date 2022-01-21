711be356b04751b3981c8feb293837477c400dae
eya-98 <eya.nani@sesame.com.tn>
2022-01-21 20:08:12 +0100

    add user

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,6 +33,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user"""
         from user import User
+
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
