92fa80c7b5cca137ec71e286c9e7d3986b6bfab7
eya-98 <eya.nani@sesame.com.tn>
2022-01-23 11:54:21 +0100

    setup a basic Flask app

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+"""setup a basic Flask app"""
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """render html template"""
     return render_template('0-index.html')
+
 
 if  __name__ == "__main__":
     app.run()
