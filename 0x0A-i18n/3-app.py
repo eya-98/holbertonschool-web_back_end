a247df6bc4c0a4ec044f00e02863cc1d55f85df9
eya-98 <eya.nani@sesame.com.tn>
2022-01-24 16:41:08 +0100

    generate .mo file

---
+++ 0x0A-i18n/3-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""setup a basic Flask app"""
+from urllib import request
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask_babel import _
+app = Flask(__name__)
+
+
+class Config:
+    """language configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object("1-app.Config")
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """language best match"""
+    return request.accept_languages.best_match(
+        app.config[1-app.Config.Languages])
+
+
+@app.route('/')
+def index():
+    """render html template"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
