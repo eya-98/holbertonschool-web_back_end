24319a0d6d97b24c7b8290706794a27baa42da14
eya-98 <eya.nani@sesame.com.tn>
2022-01-24 13:29:26 +0100

    best match language

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""setup a basic Flask app"""
+from urllib import request
+from flask import Flask, render_template
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+class Config:
+    """language configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object("1-app.Config")
+babel = Babel(app)
+
+@babel.localeselector
+def get_local():
+    return request.acceptlanguages.best_match(app.config[1-app.Config.languages])
+
+@app.route('/')
+def index():
+    """render html template"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
