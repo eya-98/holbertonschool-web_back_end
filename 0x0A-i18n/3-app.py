10c586b614383d55d06fd7d2aa07c450cce4f92d
eya-98 <eya.nani@sesame.com.tn>
2022-01-24 16:45:02 +0100

    generate .mo file

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -28,7 +28,7 @@ def get_locale():
 @app.route('/')
 def index():
     """render html template"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
