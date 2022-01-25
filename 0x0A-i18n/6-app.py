bac0003ff5166f5fa1ddd9d42b2b5cb12f659497
eya-98 <eya.nani@sesame.com.tn>
2022-01-25 11:13:58 +0100

    locale config

---
+++ 0x0A-i18n/6-app.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+"""setup a basic Flask app"""
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+app = Flask(__name__)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """language configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object("3-app.Config")
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """language best match"""
+    if request.args.get('locale') in Config.LANGUAGES:
+        return request.args.get('locale')
+    if g.user.get('locale') in Config.LANGUAGES:
+        return g.user.get('locale')
+    if request.headers.get('locale') in Config.LANGUAGES:
+        return request.headers.get('locale')
+    return 'en'
+
+
+@app.route('/')
+def index():
+    """render html template"""
+    return render_template('5-index.html')
+
+
+def get_user():
+    """get user"""
+    userID = request.args.get('login_as')
+    if userID:
+        if int(userID) in users.keys():
+            return users.get(int(userID))
+    return None
+
+
+@app.before_request
+def before_request():
+    """set global variable"""
+    if get_user():
+        g.user = get_user()
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
