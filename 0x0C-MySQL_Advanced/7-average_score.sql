36280c74b08e9b786dea3384547e1e773de553e0
eya-98 <eya.nani@sesame.com.tn>
2022-01-28 19:06:48 +0100

    average score

---
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,10 @@
+-- dummy comment dummy comment dummy comment
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser ;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+    WHERE id = user_id;
+END$$
+DELIMITER ;
