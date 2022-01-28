3b4164aedcb4cb947d056db31390666e52447d95
eya-98 <eya.nani@sesame.com.tn>
2022-01-28 19:13:37 +0100

    average weighted score

---
+++ 0x0C-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,19 @@
+-- creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    DROP VIEW IF EXISTS users_stats;
+    CREATE VIEW users_stats AS
+    SELECT cor.user_id, (SUM(cor.score * pro.weight) / SUM(pro.weight)) as arg 
+    FROM corrections AS cor
+    JOIN projects AS pro ON pro.id = cor.project_id
+    GROUP BY cor.user_id;
+
+    UPDATE users AS usr
+    SET average_score = (
+        SELECT arg FROM users_stats WHERE usr.id = user_id
+    );
+    DROP VIEW IF EXISTS users_stats;
+END$$
+DELIMITER ;
